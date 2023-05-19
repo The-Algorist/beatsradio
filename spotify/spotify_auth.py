bd955c1d7bf19aa04fd724edb2b69ffc2a1ce979
Warui Wanjiru <83602292+The-Algorist@users.noreply.github.com>
2023-05-19 21:16:42 +0300

    Update spotify_auth.py

--- spotify/spotify_auth.py
+++ spotify/spotify_auth.py
@@ -2,8 +2,7 @@ import spotipy
 import config
 
 # Get your Spotify client ID and secret from https://developer.spotify.com/dashboard/
-CLIENT_ID = 'f42f01c439ec4787b347663c39aa9eaf'
-CLIENT_SECRET = '674389a5d70d4c09abd0fad576616c40'
+
 REDIRECT_URI = 'http://localhost:5000/callback'
 
 # Create a SpotifyOAuth object
